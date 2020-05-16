21cee4d0c67e54bc73ab3e554d215f07cbecebc2
minjijo <story3697@gmail.com>
2020-05-16 20:52:20 +0900

    Using Router

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on: http://localhost:${PORT}`);
-
+const handleHome = (req, res) => res.send("Hello");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
-app.listen(PORT, handleListening);
+
+app.get("/", handleHome);
+app.use("/user", userRouter);
+
+export default app;
